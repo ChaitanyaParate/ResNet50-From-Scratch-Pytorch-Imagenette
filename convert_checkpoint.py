2cfbc3bbb56048d79986d30789ac4e63cb53c27c
ChaitanyaParate <paratechaitanya1@gmail.com>
2025-11-27 17:24:13 +0530

    Initial Commit

--- convert_checkpoint.py
+++ convert_checkpoint.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 from model import ResNet
 
@@ -27,31 +26,3 @@ model.load_state_dict(checkpoint["state_dict"])
 
 torch.save(model.state_dict(), "ResNet.pth")
 print("Converted to ResNet.pth successfully!")
-=======
-import torch
-from model import ResNet
-
-DEVICE = "cuda"
-
-checkpoint = torch.load("my_checkpoint.pth.tar", map_location=DEVICE)
-
-print("=> Checkpoint keys:", checkpoint.keys())
-
-model = ResNet(num_classes=10).to(DEVICE)
-print("=> Checkpoint state_dict keys:")
-for k in checkpoint["state_dict"].keys():
-    print(k)
-    break  # remove this to see all if needed
-
-print("\n=> Model state_dict keys:")
-for k in model.state_dict().keys():
-    print(k)
-    break  # remove this to see all if needed
-
-_ = model(torch.randn(2, 3, 224, 224).to(DEVICE))
-
-model.load_state_dict(checkpoint["state_dict"])
-
-torch.save(model.state_dict(), "ResNet.pth")
-print("Converted to ResNet.pth successfully!")
->>>>>>> a068dfbb5a14230540681b7fe9b4b8dbe2ae5190
